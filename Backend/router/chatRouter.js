5897f2a751caf4e2247e1fb075fe528f709531b6
Niteshkumar <nkumar35101@gmail.com>
2022-11-21 21:06:43 +0530

    all endpoints of chat will be created e.g - creategroup, grouprename, addusertogroup etc...

--- Backend/router/chatRouter.js
+++ Backend/router/chatRouter.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 const router = new express.Router();
 const userAuth = require("../middlewares/authMiddleware");
@@ -6,9 +7,8 @@ const User = require("../models/userModel");
 
 
 
-
-
 router.use("/onetoone", userAuth);
+router.use("/group", userAuth);
 
 router.post("/onetoone", async(req, res) => {
 
@@ -64,14 +64,149 @@ router.post("/onetoone", async(req, res) => {
  });
 
 
+// ------------------------------------------------------
+
+router.get("/onetoone", async (req, res) => {
+
+  try {
+    const chatings = await Chat.find({
+      user: { $elemMatch: { $eq: req.user._id } },
+    })
+      .populate("users", "-password")
+      .populate("latestMessage")
+      .populate("groupAdmin", "-password")
+      .sort({ updatedAt: -1 })
+      .then(async(results) => {
+      results = await User.populate(results, {
+        path: "latestMessage.sender",
+        select: "name email",
+      });
+        res.status(200).send(results)
+    })
+  }
+  catch (e) {
+    res.status(404).send({ data: e });
+  }
+  
+
+ });
 
 
-router.get("/", (req, res) => {});
-router.post("/group", (req, res) => {});
-router.put("/rename", (req, res) => {});
-router.put("/groupremove", (req, res) => { });
-router.put("/groupadd", (req, res) => {});
+// -------------------------------------------------------
 
+
+
+router.post("/creategroup", async(req, res) => {
+
+  const { allusers, name } = req.body;
+
+  if (!allusers || !name) {
+    return res.status(400).send({ "message": "please fill all the fields" });
+  }
+
+  var users = JSON.parse(allusers);
+
+  if (users.length < 2) {
+    return res.status(400).send({ message: "more than two peoples are required for group chat" });
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupchat = await Chat.create({
+
+      chatName: name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+      
+    });
+
+    const fullGroupChat = await Chat.find({ _id: groupchat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    
+    res.status(200).send(fullGroupChat);
+  }
+  catch (e) {
+     res.status(404);
+     throw new Error(error.message);
+  }
+
+ });
+
+
+
+ // --------------------------------------------------------
+
+
+router.put("/grouprename", async(req, res) => {
+
+  const { chatId, chatName } = req.body;
+
+  const updateChat = await Chat.findByIdAndUpdate(
+    chatId,
+    { chatName: chatName },
+    { new: true }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updateChat) {
+    res.status(404).send("chat not found");
+  }
+  else {
+    res.status(200).send(updateChat);
+  }
+
+ });
+
+
+
+ // --------------------------------------------------------------
+
+router.put("/addusergroup", async(req, res) => { 
+  const { chatId, userId } = req.body;
+
+  const addedToGroup = await Chat.findByIdAndUpdate(
+    chatId,
+    { $push: { users : userId }},
+    { new: true }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!addedToGroup) {
+    res.status(404).send("chat not found");
+  }
+  else {
+    res.status(200).send(addedToGroup);
+  }
+});
+ 
+
+
+// -------------------------------------------------------------
+
+
+router.put("/removeuserfromgroup", async(req, res) => { 
+  const { chatId, userId } = req.body;
+
+  const removedFromGroup = await Chat.findByIdAndUpdate(
+    chatId,
+    { $pull: { users : userId }},
+    { new: true }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removedFromGroup) {
+    res.status(404).send("chat not found");
+  }
+  else {
+    res.status(200).send(removedFromGroup);
+  }
+});
 
 
 
